ab01d0d79c46e845a2b6ce0221a902ce11a3ca74
Krisz <rybarkristik@gmail.com>
2023-09-16 13:24:35 +0200

    made the game loop, fps set to 144

--- src/com/bluu/cat/Main.java
+++ src/com/bluu/cat/Main.java
@@ -1,2 +1,24 @@
-package com.bluu.cat;public class Main {
+package com.bluu.cat;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame gameWindow = new JFrame();
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.setResizable(true);
+        gameWindow.setTitle("Bluuland");
+
+
+        GamePanel gamePanel = new GamePanel();
+        gameWindow.add(gamePanel);
+
+        gameWindow.pack();
+
+        gameWindow.setLocationRelativeTo(null);
+        gameWindow.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
 }
